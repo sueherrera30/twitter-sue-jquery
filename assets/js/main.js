580a1a58bb7397f74f29a9c097b1775607f52a6f
sue martinez <sueherrera30@gmail.com>
2017-06-04 02:17:56 +0000

    elimina pero no se ve botecito

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,45 @@
-
-(function () {
-var contador =0;
-
-var cargarPagina = function(){
-$("#formulario").submit(enviarTweet);
-};
-
-
-var enviarTweet = function(e){
-  e.preventDefault();
- /*traemos elementos*/
-  var $btnEnviar = $("#btnEnviar");
-  var $contenedorTweet = $("#area-escribir");
-  var $recipienteTweets = $("#recipiente-tweets");
-  /*obtenemos el valor del mensaje ingresado*/
-  var $tweet = $contenedorTweet.val();
-  
-  /*creamos elemento de forma dinamica*/
-  var $contenedorTweetNuevo = $("<div/>",{"class": "card-panel"}); 
-  var $palomita = $("<input type='checkbox' />");
-  var $postTexto = $("<label />");
-  var identificador = "marcador-" + contador;	
-  $palomita.id = identificador;
+$(document).ready(function(){
+ var contador = 0;
+ var $btnEnviar = $("#btnEnviar");
 	
-		contador++;
-	};
-$(document).ready(cargarPagina);
-})();
+ var enviarTweet = function(){
+  /*traemos elementos*/
+  
+   var $contenedorTweet = $("#area-escribir");
+   var $recipienteTweets = $("#recipiente-tweets");
+	
+   /*obtenemos el valor del mensaje ingresado*/
+   var $tweet = $contenedorTweet.val();
+  
+   /*creamos elemento de forma dinamica*/
+   var $contenedorTweetNuevo = $("<div/>",{ "class": "card-panel col s4 offset-s4 teal-text text-lighten-2"});
+   
+   var $fila = $("<div/>", { "class": "row"});
+	 
+	 var $imgPajarito = $('<img>',{'class': 'responsive-img pajarito'});
+	 $imgPajarito.attr('src', 'assets/img/logo2.png');
+	 
+	 var $eliminar = $("<i/>",{"class": "material-icons eliminar pink-text"});
+	 /*var $almacenTextoTweet = $("<p/>");*/
+	 
+	 $eliminar.text("delate");
+	/* $almacenTextoTweet.text($tweet);*/
+	 $contenedorTweetNuevo.append($imgPajarito);
+	 $contenedorTweetNuevo.append($tweet);
+     $contenedorTweetNuevo.append($eliminar);
+	 $fila.append($contenedorTweetNuevo);
+      $recipienteTweets.prepend($fila);
+	 
+  var eliminarTweet = function (){
+    $(this).parent().remove();
+  }
+  
+	  var $btnEliminarTweet = $('.eliminar'); 
+	  $btnEliminarTweet.click(eliminarTweet);
+};	 
+	
+ $btnEnviar.click(enviarTweet);
+
+	 
+});
+
