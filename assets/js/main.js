ca04fa0808d5031b03de04a21fb1604887e3e774
sue martinez <sueherrera30@gmail.com>
2017-06-04 23:28:27 +0000

    contador de colores

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,31 +14,59 @@ $(document).ready(function(){
    /*creamos elemento de forma dinamica*/
    var $contenedorTweetNuevo = $("<div/>",{ "class": "card-panel col s4 offset-s4 teal-text text-lighten-2"});
    
-   var $fila = $("<div/>", { "class": "row"});
-	 
+     var $fila = $("<div/>", { "class": "row"});
 	 var $imgPajarito = $('<img>',{'class': 'responsive-img pajarito'});
-	 $imgPajarito.attr('src', 'assets/img/logo2.png');
-	 
-	 var $eliminar = $("<i/>",{"class": "material-icons eliminar pink-text"});
-	 /*var $almacenTextoTweet = $("<p/>");*/
-	 
-	 $eliminar.text("delate");
-	/* $almacenTextoTweet.text($tweet);*/
+	 $imgPajarito.attr('src', 'assets/img/logo2.png'); 
+	 var $eliminar = $("<i/>",{"class":"material-icons eliminar teal-text"});
+	 $eliminar.text("delete");
 	 $contenedorTweetNuevo.append($imgPajarito);
 	 $contenedorTweetNuevo.append($tweet);
-     $contenedorTweetNuevo.append($eliminar);
+     $contenedorTweetNuevo.append($eliminar); 
 	 $fila.append($contenedorTweetNuevo);
-      $recipienteTweets.prepend($fila);
-	 
+     $recipienteTweets.prepend($fila);
+	 $contenedorTweet.val("");
+	
+  /*funcion que elimina tweets*/
   var eliminarTweet = function (){
     $(this).parent().remove();
-  }
-  
-	  var $btnEliminarTweet = $('.eliminar'); 
-	  $btnEliminarTweet.click(eliminarTweet);
+	    
+  };
+   var $btnEliminarTweet = $('.eliminar'); 
+   $btnEliminarTweet.click(eliminarTweet);
+
+	
+	 
 };	 
 	
+  var contadorPalabras = function(){
+	  var $contenedorTweet = $("#area-escribir");
+	  var $contador = $contenedorTweet.val().length;
+	  var $contenedorContador = $("#contador-contenedor");
+	  $contenedorContador.text($contador +"/140 caracteres");
+	  
+	  if ($contador <= 119){
+			$contenedorContador.addClass("green");
+	  }
+	  if ($contador >= 120){
+			$contenedorContador.removeClass("green").addClass("yellow lighten-2");
+	  }
+	  
+	  if ($contador >= 130){
+		  $contenedorContador.removeClass("yellow lighten-2").addClass("amber");
+	       }
+	  
+		   if ($contador >= 140){
+			$contenedorContador.removeClass("amber").addClass("red darken-1");
+		   }
+		
+	};
+	
+	  var $contenedorTweet = $("#area-escribir");
+	  $contenedorTweet.keyup(contadorPalabras);
+	
  $btnEnviar.click(enviarTweet);
+	
+ /*funcion contador*/
 
 	 
 });
